8ac8e8da0ee2c8a04c93d8e775b5db08cefa6dbd
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-01 16:28:02 -0400

    bonus2 complete

---
+++ bonus2.py
@@ -0,0 +1,19 @@
+# number guessing game
+# import random module for randint func
+from random import randint
+
+# variables for loop counter, guess, and random number
+counter = 0
+guess = ''
+randNum = randint(1, 10)
+
+#logic 
+while counter < 5:
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess == randNum:
+        print("You won!")
+        break
+    if guess != randNum:
+        print("Wrong.  Guess again.")
+    counter += 1
+print("Run the program to play again")
