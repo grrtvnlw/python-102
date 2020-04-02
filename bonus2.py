ce21aa13e4f05f763b3fac4fb32c125a3ec7a68b
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-01 20:37:56 -0400

    refactored bonus2.py

--- bonus2.py
+++ bonus2.py
@@ -4,16 +4,15 @@ from random import randint
 
 # variables for loop counter, guess, and random number
 counter = 0
-guess = ''
 rand_num = randint(0, 10)
 
 # logic 
 while counter < 5:
     guess = int(input("Guess a number between 0 and 10: "))
     if guess == rand_num:
-        print("You won! :)")
+        print("You won! :D")
         break
-    if guess != rand_num:
+    else:
         print("Wrong. :(")
     counter += 1
-print("Run the program to play again. :D")
+print("Run the program to play again. :)")
